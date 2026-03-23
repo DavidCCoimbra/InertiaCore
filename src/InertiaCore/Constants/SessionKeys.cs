56d51b9da1afc6c1bf1d2ea9e6eb7f6e0b1a5faa
DavidCCoimbra <git@davidcoimbra.dev>
2026-03-23 09:24:37 +0000

    [DCO-11] feat: SessionKeys constants

---
+++ src/InertiaCore/Constants/SessionKeys.cs
@@ -0,0 +1,23 @@
+namespace InertiaCore.Constants;
+
+/// <summary>
+/// Session and TempData key constants used by the Inertia protocol.
+/// </summary>
+public static class SessionKeys
+{
+    /// <summary>
+    /// Session key for clearing the Inertia history.
+    /// </summary>
+    public static readonly string ClearHistory = "inertia.clear_history";
+    
+    /// <summary>
+    /// Session key for flash data.
+    /// </summary>
+    public static readonly string FlashData = "inertia.flash_data";
+    
+    /// <summary>
+    /// Session key for preserving the URL fragment.
+    /// </summary>
+    public static readonly string PreserveFragment = "inertia.preserve_fragment";
+}
+
